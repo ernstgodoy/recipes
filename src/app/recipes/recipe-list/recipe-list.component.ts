1a3ebf6dd72012d9446d408dcd9ab6bdd8a2842b
ernesto godoy <ernesto@ernestos-MacBook-Pro.local>
2020-04-24 16:29:46 -0700

    list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
-
-  constructor() { }
-
-  ngOnInit(): void {
+  recipes: Recipe[] = [
+    new Recipe('Test','Test recipe','https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-750x563.jpg'),
+    new Recipe('Test','Test recipe','https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-750x563.jpg')
+  ]
+  
+  constructor() { 
   }
+  
+  ngOnInit(): void {
+    
+    console.log(this.recipes)
 
+  }
 }
